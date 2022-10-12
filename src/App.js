433afc80cde6905766b680538c6cbf3c57e48f18
Aleyna Yıldız <aleyyldz9@gmail.com>
2022-10-12 22:05:51 +0300

    anasayfa sekmesi oluşturuldu

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navigation from './components/Navigation';
+import Carousel from './components/Carousel';
+import Card from './components/Card';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <div>
+        <Carousel/>
+      </div>
+      <div className='CardStyle'>
+        <Card className='Card1'/>
+        <Card className='Card1'/>
+        <Card className='Card1'/>
+      </div>
+      <div>
+        <Footer/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
